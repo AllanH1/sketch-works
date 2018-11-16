2c329e56f17dcf7585b987314df2782784bda11e
Allan Hernandez <allanhernandezR2H@rv-clt-allhernandez.redventures.net>
2018-11-16 17:07:26 -0500

    adds animations to cards and top navigation

---
+++ Architecture-concept/JS/animations.js
@@ -0,0 +1,21 @@
+const searchText = document.querySelector(".nav__link__text--search");
+const searchIcon = document.querySelector(".nav__link__icon--search");
+const searchInput = document.querySelector(".search-container__input");
+const closeInputIcon = document.querySelector(".search-container__close-icon");
+
+const openSearch = e => {
+  e.preventDefault();
+  searchInput.classList.add("search-container__input--open");
+  searchText.classList.add("nav__link__text--search--open");
+  closeInputIcon.classList.add("search-container__close-icon--open");
+};
+
+const closeSearch = () => {
+  searchInput.classList.remove("search-container__input--open");
+  searchText.classList.remove("nav__link__text--search--open");
+  closeInputIcon.classList.remove("search-container__close-icon--open");
+};
+
+searchText.addEventListener("click", openSearch);
+searchIcon.addEventListener("click", openSearch);
+closeInputIcon.addEventListener("click", closeSearch);
